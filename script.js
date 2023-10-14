148bbceb5ca8126e4c172bc58a5dbf9f39dbd64f
Muattar <avraevam96@gmail.com>
2023-10-14 10:43:47 +0500

    js 1 hw

---
+++ script.js
@@ -0,0 +1,43 @@
+var name = prompt("Ismingizni kiriting.")
+console.log("Sizning ismingiz " + name);
+var age = prompt("Yoshingizni kiriting.")
+console.log("Sizning yoshingiz " + age + " da");
+
+var misol = +prompt(" Misolni yeching 6+10=?")
+if (misol === 16) {
+    alert("Sizning javob tog'ri 16")
+} else {
+    alert("Sizning javob notog'ri")
+    console.log("Sizning javob notog'ri " + misol + " Tog'ri javob 16")
+}
+
+var misol1 = +prompt("Misolni yeching 15-6=?");
+if (misol1 === 9) {
+    alert("Sizning javob tog'ri 9")
+} else {
+    alert("Sizning javob notog'ri")
+    console.log("Sizning javob notog'ri " + misol1 + " Tog'ri javob 9")
+}
+
+var misol2 = +prompt(" Misolni yeching 9*5=?")
+if (misol2 === 45) {
+    alert("Sizning javob tog'ri 45")
+} else {
+    alert("Sizning javob notog'ri")
+    console.log("Sizning javob notog'ri " + misol2 + " Tog'ri javob 45")
+}
+
+var misol3 = +prompt("Misolni yeching 10/5=?")
+if (misol3 === 2) {
+    alert("Sizning javob tog'ri 2")
+} else {
+    alert("Sizning javob notog'ri")
+    console.log("Sizning javob notog'ri " + misol3 + " Tog'ri javob 2")
+}
+var misol4 = +prompt("Misolni yeching 25%3=?")
+if (misol4 === 1) {
+    alert("Sizning javob tog'ri 1")
+} else {
+    alert("Sizning javob notog'ri")
+    console.log("Sizning javob notog'ri " + misol4 + " Tog'ri javob 1")
+}
